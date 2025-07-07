129dc05c4cd6b891dd28061bdef04352794e7919
Dana Sharon <whosedreamisthis@gmail.com>
2025-07-07 13:58:32 -0700

    installed routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 function App() {
 	console.log('generatePalette', generatePalette(seedColors[4]));
 	return (
-		<div>
-			<Palette palette={generatePalette(seedColors[4])} />
-		</div>
+		<Routes>
+			<Route path="/" element={<h1>PALETTE LIST GOES HERE</h1>} />
+			<Route path="/palette/:id" element={<h1>Individual Palette</h1>} />
+		</Routes>
+		// <div>
+		// 	<Palette palette={generatePalette(seedColors[4])} />
+		// </div>
 	);
 }
 
