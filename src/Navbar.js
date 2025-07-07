22de78776e58a3966f0c1f29135a2d68e1dc06d5
Dana Sharon <whosedreamisthis@gmail.com>
2025-07-07 12:51:12 -0700

    add select and material UI

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+
 class Navbar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { format: 'hex' };
+		this.changeFormat = this.changeFormat.bind(this);
+	}
+	changeFormat(e) {
+		this.setState({ format: e.target.value });
+		this.props.changeFormat(e.target.value);
+	}
 	render() {
 		const { level, changeLevel } = this.props;
+		const { format } = this.state;
 		return (
 			<header className="Navbar">
 				<div className="logo">
@@ -22,6 +37,37 @@ class Navbar extends Component {
 						/>
 					</div>
 				</div>
+				<div className="select-container">
+					<Select
+						labelId="demo-simple-select-label"
+						id="demo-simple-select"
+						value={format}
+						label="Age"
+						onChange={this.changeFormat}
+					>
+						<MenuItem value={'hex'}>HEX - #ffffff</MenuItem>
+						<MenuItem value={'rgb'}>
+							RGB - rgb(255,255,255)
+						</MenuItem>
+						<MenuItem value={'rgba'}>
+							RGBA- rgba(255,255,255,1.0)
+						</MenuItem>
+					</Select>
+				</div>
+				{/* <FormControl fullWidth>
+					<InputLabel id="demo-simple-select-label">Age</InputLabel>
+					<Select
+						labelId="demo-simple-select-label"
+						id="demo-simple-select"
+						value={12}
+						label="Age"
+						onChange={() => {}}
+					>
+						<MenuItem value={10}>Ten</MenuItem>
+						<MenuItem value={20}>Twenty</MenuItem>
+						<MenuItem value={30}>Thirty</MenuItem>
+					</Select>
+				</FormControl> */}
 			</header>
 		);
 	}
--- src/Palette.js
+++ src/Palette.js
@@ -4,17 +4,31 @@ import Navbar from './Navbar';
 import './Palette.css';
 function Palette({ palette }) {
 	const [level, setLevel] = useState(500);
+	const [format, setFormat] = useState('hex');
+
 	const colorBoxes = palette.colors[level].map((color) => {
 		return (
-			<ColorBox key={color.id} background={color.hex} name={color.name} />
+			<ColorBox
+				key={color.id}
+				background={color[format]}
+				name={color.name}
+			/>
 		);
 	});
 	function changeLevel(level) {
 		setLevel(level);
 	}
+	function changeFormat(val) {
+		setFormat(val);
+		console.log(val);
+	}
 	return (
 		<div className="Palette">
-			<Navbar level={level} changeLevel={changeLevel} />
+			<Navbar
+				level={level}
+				changeLevel={changeLevel}
+				changeFormat={changeFormat}
+			/>
 			<div className="Palette-colors">{colorBoxes}</div>
 			{/* footer */}
 		</div>
