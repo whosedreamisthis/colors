d4960ade2b8563aec6ad8f540c7e2e0d8ec1e6ed
Dana Sharon <whosedreamisthis@gmail.com>
2025-07-07 14:16:05 -0700

    fix palette route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Routes, Route, useParams } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
+function findPalette(id) {
+	return seedColors.find((palette) => palette.id === id);
+}
+function PalettePage() {
+	// useParams() is called INSIDE a component that is rendered BY a Route
+	const { id } = useParams();
+	console.log('ID from useParams in PalettePage:', id); // Debug here
+
+	// Use the id to find and generate the palette
+	const foundPalette = findPalette(id);
+	console.log(foundPalette);
+	const fullPalette = foundPalette ? generatePalette(foundPalette) : null;
+
+	if (!fullPalette) {
+		// Handle case where palette is not found (e.g., show a 404 page)
+		return <h1>Palette Not Found!</h1>;
+	}
+
+	return <Palette palette={fullPalette} />;
+}
+
 function App() {
-	console.log('generatePalette', generatePalette(seedColors[4]));
+	const { id } = useParams();
+	const foundPalette = findPalette(id);
 	return (
 		<Routes>
 			<Route path="/" element={<h1>PALETTE LIST GOES HERE</h1>} />
-			<Route path="/palette/:id" element={<h1>Individual Palette</h1>} />
+			<Route path="/palette/:id" element={<PalettePage />} />
 		</Routes>
 		// <div>
 		// 	<Palette palette={generatePalette(seedColors[4])} />
