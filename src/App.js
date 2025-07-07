ea0c58fb05406482c58c28c57ff6ae38029ea356
Dana Sharon <whosedreamisthis@gmail.com>
2025-07-06 19:30:46 -0700

    create react app, added palette

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Palette from './Palette';
+import seedColors from './seedColors';
+function App() {
+	return (
+		<div>
+			<Palette {...seedColors[4]} />
+		</div>
+	);
+}
+
+export default App;
